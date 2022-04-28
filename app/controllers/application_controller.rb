fac5a02a868412386a560e047466117a1d09c1b0
Michael Chung <mikechung89@gmail.com>
2022-04-28 01:10:11 -0400

    commit tree b2725102702af600f51fc0f287c14167ed569c04

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :render_no_found_response
+
+    private
+
+    def render_no_found_response
+        render json: {error: "Item not found" }, status: :not_found
+    end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,20 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_no_found_response
 
   def index
     items = Item.all
     render json: items, include: :user
+  end
+
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+
+  private
+
+  def render_no_found_response
+    render json: {error: "Item not found" }, status: :not_found
   end
 
 end
